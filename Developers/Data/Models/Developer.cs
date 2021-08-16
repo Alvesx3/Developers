01cc0b1ff0b41565205cf8b1476822384ab72990
lorenzo <lomichelotti@hotmail.com>
2021-08-16 16:10:03 -0300

    Create Composition between Contact and Address classes. Added the Developer Model. #21

---
+++ Developers/Data/Models/Developer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Developer : BaseModel
+    {
+        //esta classe é uma composição das demais <------
+
+        //public Profile profile { get; set; }
+        //public User user { get; set; }
+        //public Contact contact { get; set; }
+    }
+}
