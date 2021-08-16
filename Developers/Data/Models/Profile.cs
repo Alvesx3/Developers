46130601308d57961a6e3cc508b2f04c9bb7513c
Camille Ouriques <mille.ouriques@gmail.com>
2021-08-16 14:43:00 -0300

    Created Profile Model Closed #4

---
+++ Developers/Data/Models/Profile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Profile : BaseModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
