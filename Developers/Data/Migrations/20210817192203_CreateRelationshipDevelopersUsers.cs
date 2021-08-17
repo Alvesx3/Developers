c93b4506c831db13a0ceddb702cf303dea39ce6c
lorenzo <lomichelotti@hotmail.com>
2021-08-17 16:22:23 -0300

    Added Migrations between the Developers Model and the Users Model

---
+++ Developers/Data/Migrations/20210817192203_CreateRelationshipDevelopersUsers.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class CreateRelationshipDevelopersUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "userId",
+                table: "Developers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Developers_userId",
+                table: "Developers",
+                column: "userId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Developers_Users_userId",
+                table: "Developers",
+                column: "userId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Developers_Users_userId",
+                table: "Developers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Developers_userId",
+                table: "Developers");
+
+            migrationBuilder.DropColumn(
+                name: "userId",
+                table: "Developers");
+        }
+    }
+}
--- Developers/Data/Models/Developer.cs
+++ Developers/Data/Models/Developer.cs
@@ -11,7 +11,7 @@ namespace Data.Models
         //esta classe é uma composição das demais <------
 
         public Profile profile { get; set; }
-        //public User user { get; set; }
+        public User user { get; set; }
         //public Contact contact { get; set; }
     }
 }
